b2e85fa716aaf4c7c421e862b6fcb49013c92c91
Connor Imes <connor.k.imes@gmail.com>
2018-02-06 17:21:42 -0600

    Add setters in context for period and model; also make constraint a variable instead of a constant

--- Sources/FASTControllerContext.swift
+++ Sources/FASTControllerContext.swift
@@ -7,20 +7,35 @@
 
 /// Stores user-defined parameters for the `FASTController`.
 internal class FASTControllerContext {
-    /// The controller model.
-    let model: FASTControllerModel
     /// The constraint goal for measure at index `constraintMeasureIdx`.
-    let constraint: Double
+    var constraint: Double
     /// The index of the measure we are trying to meet a constraint for (must be < `FASTControllerModel.nMeasures`).
     let constraintMeasureIdx: Int
-    /// The window period (number of application jobs between controller decisions).
-    let period: UInt32
     /// The type of optimization to perform (minimize or maximize).
     let optType: FASTControllerOptimizationType
     /// A callback that computes the value we are trying to optimize.
     let ocb: GetCostOrValueFunction
+    /// The controller model.
+    var model: FASTControllerModel {
+        get {
+            return self.model
+        }
+        set {
+            assert(self.model.nMeasures == newValue.nMeasures, "Number of measures cannot change")
+            self.setModel(newValue)
+        }
+    }
     /// the constraint model - sorted and normalized array for model[i][constraintMeasureIdx]
-    let xupModel: [Double]
+    private(set) var xupModel: [Double]!
+    /// The window period (number of application jobs between controller decisions).
+    var period: UInt32 {
+        get {
+            return self.period
+        }
+        set {
+            self.setPeriod(newValue)
+        }
+    }
 
     /// Create a `FASTControllerContext` - performs assertions on parameter value ranges
     init(model: FASTControllerModel,
@@ -29,15 +44,18 @@ internal class FASTControllerContext {
          period: UInt32,
          optType: FASTControllerOptimizationType,
          ocb: @escaping GetCostOrValueFunction) {
-        assert(period > 0, "period must be > 0")
         assert(constraintMeasureIdx >= 0, "constraintMeasureIdx must be >= 0")
-        assert(constraintMeasureIdx < model.nMeasures, "constraintMeasureIdx must be < \(model.nMeasures)")
-        self.period = period
         self.constraint = constraint
         self.constraintMeasureIdx = constraintMeasureIdx
         self.optType = optType
-        self.model = model
         self.ocb = ocb
+        setModel(model)
+        setPeriod(period)
+    }
+
+    private func setModel(_ model: FASTControllerModel) {
+        assert(model.nMeasures > self.constraintMeasureIdx,
+               "model.nMeasures (\(model.nMeasures)) must be > constraintMeasureIdx (\(self.constraintMeasureIdx))")
         // create the xupModel, normalized by the constraint measure
         var xupModel: [Double] = [Double](repeating: 0.0, count: model.nEntries)
         xupModel[0] = 1.0
@@ -47,6 +65,13 @@ internal class FASTControllerContext {
                    "Model not sorted by constraintMeasureIdx at entry=\(i)")
             xupModel[i] = model.measures[i][constraintMeasureIdx] / model.measures[0][constraintMeasureIdx]
         }
+        self.model = model
         self.xupModel = xupModel
     }
+
+    private func setPeriod(_ period: UInt32) {
+        assert(period > 0, "period must be > 0")
+        self.period = period
+    }
+
 }
